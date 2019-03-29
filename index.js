3b4dc536c10bfae558f7aca878c76cd71ce16352
jskmd <41142681+jskmd@users.noreply.github.com>
2019-03-29 18:44:11 +0800

    0.0.1
    新的文件

---
+++ index.js
@@ -0,0 +1,24 @@
+//加法运算
+exports.sum=function (){
+  var res=0;
+  for(var i=0;i<arguments.length;i++){
+    res+=arguments[i];
+  }
+  return res;
+};
+//减法运算
+exports.sub=function(a, b){
+  return a-b;
+}
+//乘法运算
+exports.mul=function(){
+  var res=0;
+  for(var i=0;i<arguments.length;i++){
+    res=res*arguments[i];
+  }
+  return res;
+}
+//除法运算
+exports.div=function (a, b){
+  return a/b;
+};
